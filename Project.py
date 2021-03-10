84daae8a1f3aa4d4268820a7704e509055444688
John Kim <johnkim@Johns-MBP.home>
2021-03-10 08:05:51 -0500

    apply changes to service class

--- Project.py
+++ Project.py
@@ -7,6 +7,7 @@ services = []
 
 class Customer():
     #constructor
+    #
     #include paramaters such as first name, last name, address, balance
     #type of account
     #withdraw/deposit method
@@ -17,7 +18,7 @@ class Customer():
         self.address = address
         self.balance = balance
         customers.append(self.__dict__)
-        customers.append()
+        
         #setter method
         
         #input property decorators for setter
@@ -27,7 +28,8 @@ class Customer():
         #must be a string or output an error
         self.first_name = x
         self.last_name = y
-
+        customers.append(self.__dict__)
+        
         #getter method
     def get_name(self):
         return self.first_name+ " "+ self.last_name
@@ -42,17 +44,28 @@ class BankAccount(Customer):
         self.address = Customer.address
         bankAccounts.append(self.__dict__)
         
-    def deposit(self,input_amount):
-        self.balance += input_amount
-
-    def withdraw():
-        if self.balance <0:
-            print('Funds Unavailable for Withdrawal')
-        elif self.balance <= input_amount:
-            print('Funds Unavailable for Withdrawal')
-        else:
-            self.balance = self.balance - input_amount
-            print("Your Updated Balance: $",self.balance)
+    def deposit(self):
+        try:
+            input_amount = input("Input an amount to deposit: ")
+            float_added_input = float(input_amount)
+            self.balance += input_amount
+            bankAccounts.append(self.__dict__)
+        except ValueError:
+            print("You provided an invalid amount")
+    def withdraw(self):
+        try:
+            input_amount = input("Input an amount to withdraw: ")
+            float_added_input = float(input_amount)
+            if self.balance <0:
+                print('Funds Unavailable for Withdrawal')
+            elif self.balance <= input_amount:
+                print('Funds Unavailable for Withdrawal')
+            else:
+                self.balance = self.balance - input_amount
+                print("Your Updated Balance: $",self.balance)
+                bankAccounts.append(self.__dict__)
+        except ValueError:
+            ("You provided an invalid amount")
             
 class Employees():
      #first name, last name, employee id, department, salary, job status, 
@@ -68,17 +81,19 @@ class Employees():
     def give_raise(self, raise_amount):
         self.salary += raise_amount
         print("Updated Employee Salary: ",self.salary)
+        employees.append(self.__dict__)
     
     
 class Services():
 
-    def __init__(self, name, address, birth_date, social_security,credit_card=None,loan=None):
+    def __init__(self, name, address, birth_date, social_security,credit_card=None,loan=None,credit_balance=0):
         self.name = name
         self.address= address
         self.birth_date = birth_date
         self.social_security = social_security
         self.credit_card = credit_card
         self.loan = loan
+        self.credit_balance = credit_balance
         services.append(self.__dict__)
     def view_info(self):
         print("Customer Information:")
@@ -91,13 +106,42 @@ class Services():
         print("Credit Card: ")
     #setter method
     def select_credit_card(self):
-        x = input("SELECT CREDIT CARD")
+        x = input("SELECT Credit Card Option (Sapphire,Freedom,or Black)")
+        
+        if x == 'Saphire':
+            print("Congratulations you are now have a $2,500 credit line!")
+            self.credit_balance = 2500
+            services.append(self.__dict__)
+        elif x == 'Freedom':
+            print("Congratulations you are now have a $3,500 credit line!")
+            self.credit_balance = 3500
+            services.append(self.__dict__)
+        elif x == 'Black':
+            print("Congratulations you are now have a $10,000 credit line!")
+            self.credit_balance = 10000
+            services.append(self.__dict__)
+    def pay_with_credit(self,input_amount):
+        self.credit_balance += input_amount
+        services.append(self.__dict__)
+
+
+    def pay_off_credit(self,input_amount):  
+        if input_amount > self.credit_balance:
+            print("The inputted amount is greater than your current credit balance. Please try again.")
+
+        else: 
+            self.credit_balance -= input_amount
+            services.append(self.__dict__)
+
 
     
     def raise_credit_limit(self, amount):
+        print("Congratulations you have been permitted to receive a increase in your credit limit")
+        self.credit_balance += amount
+
 
     
-    #loans, credit cards
+   
 
 
 
